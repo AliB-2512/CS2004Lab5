f4a91c5bb69666e851f3983cd240460c0d326c93
Ali Bachar <alibachar@icloud.com>
2019-10-24 19:10:28 +0100

    5.2.2 Exercise 3

--- src/Lab5.java
+++ src/Lab5.java
@@ -5,8 +5,12 @@ import java.util.LinkedList;
 
 public class Lab5 {
 	public static void main(String args[]) {
+		
 		Exercise2();
+
+		Exercise3();
 	}
+	
 	public static void PrintCollection(Collection<Data> c)
 	{
 		for (Iterator<Data> iter = c.iterator(); iter.hasNext();)
@@ -17,6 +21,7 @@ public class Lab5 {
 		System.out.println();
 	}
 	public static void Exercise2()	{
+		System.out.println("Exercise 2:");
 		LinkedList<Data> array = new LinkedList<Data>();
 		Data fred = new Data("Fred",21);
 		Data jo = new Data("Jo",43);
@@ -27,5 +32,40 @@ public class Lab5 {
 		array.add(zoe);
 		array.add(2,harry);
 		PrintCollection(array);
+	}
+	public static void Exercise3() {
+		System.out.println("\nExercise 3: part 1");
+		ArrayList<Data> arrayA = new ArrayList<Data>();
+		ArrayList<Data> arrayB = new ArrayList<Data>();
+		ArrayList<Data> arrayC = new ArrayList<Data>();
+		ArrayList<Data> arrayD = new ArrayList<Data>();
+		Data fred = new Data("Fred",21);
+		Data jo = new Data("Jo",43);
+		Data zoe = new Data("Zoe",37);
+		arrayA.add(fred);
+		arrayA.add(jo);
+		arrayA.add(zoe);
+		arrayC.add(fred);
+		arrayC.add(jo);
+		arrayC.add(zoe);
+		PrintCollection(arrayA);
+		System.out.println();
+		arrayB = arrayA;
+		PrintCollection(arrayB);
+		System.out.println();
+		arrayA.remove(1); //array indexing starts at 0 not one hence why value 2 at position 1 is deleted 
+		PrintCollection(arrayB); //arrayB points towards arrayA and thus when something is removed from arrayA arrayB updates
+		System.out.println("\nExercise 3: part 2");
+		PrintCollection(arrayC);
+		System.out.println();
+		arrayD = (ArrayList<Data>)arrayC.clone(); //arrayD is a complete clone and is now independent of arrayC and how its manipulated
+		arrayC.remove(1);
+		PrintCollection(arrayC);
+		System.out.println();	
+		PrintCollection(arrayD);
+		System.out.println();
+
+
+		
 	}
 }
